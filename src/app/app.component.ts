b11e4e71becf9603db9e398a9083d6b6eebc73ab
psergioPadilha <psergiopadilha@gmail.com>
2024-09-19 18:21:29 -0300

    Atualização da navBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: "app.component.html",
   styleUrl: "app.component.scss",
 })
-export class AppComponent { }
+export class AppComponent
+{
+  public colpsarNavbar: boolean = false;
+}
